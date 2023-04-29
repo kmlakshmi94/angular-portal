762bae9b86b4670331a463416e7e782e4052bf5e
Pandeyan C <pandeyanc@Pandeyans-MacBook-Pro.local>
2023-04-30 00:29:17 +0530

    initial setup commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component ({
+    selector: 'app-ui',
+    template: '<p>hello world </p>'
+})
+
+export class AppComponent {
+
+    name = 'Angular';
+}
---
+++ src/main.ts
@@ -0,0 +1,4 @@
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { Appmodule } from "./app/app.module";
+
+platformBrowserDynamic().bootstrapModule(Appmodule);
